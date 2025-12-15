0e25e94a0fa61f3eaec9b565e2b412b02b64695f
NoobCoderDev <sonbhurra@gmail.com>
2025-12-15 15:29:29 +0530

    Updated username for pythonanywhere

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add your project directory to the Python path
-path = '/home/NoobCoderDev/promptkhajana'  # Replace 'yourusername' with your PythonAnywhere username
+path = '/home/PromptKhajana/promptkhajana'  # Replace 'yourusername' with your PythonAnywhere username
 if path not in sys.path:
     sys.path.append(path)
 
